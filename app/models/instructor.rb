d444d9c06cc7852b7abb5f55c8b608bafd86a2a7
github username <github email address>
2020-08-10 16:45:40 -0400

    all but one test passing

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -18,10 +18,28 @@ class Instructor
     end
 
     def pass_student(student, test_name)
-        if BoatingTest.all.find {|test| test.test_name && test.student}
-             
-
+            if test.student == student && test.test_name == test_name
+                BoatingTest.all.map do |test|
+                    test.status = "passed"
+                end
+            else
+                BoatingTest.new(student, test_name, "passed", self)
+            end
     end 
+    
+    def fail_student(student, test_name)
+        failed_test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+        if failed_test
+            failed_test.status = "failed"
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end 
+    end
+
+    def all_students
+        BoatingTest.all.select {|test| test.instructor == self}.map {|test| test.student}
+    end 
+        
 
     def self.all
         @@all
--- console.rb
+++ console.rb
@@ -13,6 +13,7 @@ no_crashing = spongebob.add_boating_test(spongebob, "Don't Crash 101", "pending"
 power_steering_failure = patrick.add_boating_test(patrick, "Power Steering 202", "failed", puff)
 power_steering_pass_patrick = patrick.add_boating_test(patrick, "Power Steering 201", "passed", krabs)
 power_steering_pass_spongebob = patrick.add_boating_test(spongebob, "Power Steering 207", "passed", krabs)
+turning_spongebob_pending = spongebob.add_boating_test(spongebob, "Turning", "pending", krabs)
 
 
 
